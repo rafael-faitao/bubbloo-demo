1baf74dd6ec9df4cbb9ec667c821711c71ed891c
rafael-faitao <rafaeltsd@hotmail.com>
2025-07-01 15:24:55 -0300

    feat colouring-game: add main screen and logic

--- src/components/ActivityBar.jsx
+++ src/components/ActivityBar.jsx
@@ -2,7 +2,7 @@ import "./ActivityBar.scss";
 
 export default function ActivityBar ({onClose}) {
     return (
-        <div class="activity-bar">
+        <div className="activity-bar">
             <img className="action-item trans-all hv-light" src="/assets/img/exit-activity-bar.svg" onClick={() => onClose()}/>
             <img className="action-item trans-all hv-light" src="/assets/img/note-activity-bar.svg"/>
             <div className="avatar-group">
